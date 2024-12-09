28eee9da0e9afdf46ff9904e92511273915e578f
zakiafada32 <afadazaki32@gmail.com>
2024-12-09 12:54:56 +0700

    Add translation feature with API and tests; enhance CI pipeline

--- Makefile
+++ Makefile
@@ -24,3 +24,15 @@ build:
 
 run:
 	go run cmd/main.go
+
+test:
+	go test ./... -coverprofile=coverage.out
+
+coverage:
+	go tool cover -func coverage.out
+
+min-coverage:
+	go tool cover -func coverage.out | grep "total:" | awk '{print ((int($$3) >= 80) != 1) }'
+
+report:
+	go tool cover -html=coverage.out -o cover.html
--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,19 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
+
+	"shipping-go/handlers/rest"
 )
 
 func main() {
 
-	addr := ":8081"
+	addr := ":8080"
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/hello",
-		func(w http.ResponseWriter, r *http.Request) {
-			enc := json.NewEncoder(w)
-			w.Header().Set("Content-Type", "application/json; charset=utf-8")
-			resp := Resp{
-				Language:    "English",
-				Translation: "Hello",
-			}
-			if err := enc.Encode(resp); err != nil {
-				panic("unable to encode response")
-			}
-		})
+	mux.HandleFunc("/translate/hello", rest.TranslateHandler)
 
 	log.Printf("listening on %s\n", addr)
 
